0da820eadafda36cf4f47fcafabeaf32e4ceee07
Helton Gabriel Dick <heltondick123@gmail.com>
2024-10-08 15:10:13 -0300

    Site copia nike

---
+++ js/script.js
@@ -0,0 +1,137 @@
+function calcados () {
+    let calcados = document.getElementById('calcados');
+    let roupas = document.getElementById('roupas');
+    let acc = document.getElementById('acessorios');
+    let esportes = document.getElementById('esportes');
+    let journal = document.getElementById('journal');
+    let promos = document.getElementById('promos');
+
+    console.log(calcados);
+    console.log(roupas);
+    console.log(acc);
+    console.log(esportes);
+    console.log(journal);
+    console.log(promos);
+
+    calcados.style.display = 'flex';
+    roupas.style.display = 'none';
+    acc.style.display = 'none';
+    esportes.style.display = 'none';
+    journal.style.display = 'none';
+    promos.style.display = 'none';
+}
+
+function roupas () {
+    let calcados = document.getElementById('calcados');
+    let roupas = document.getElementById('roupas');
+    let acc = document.getElementById('acessorios');
+    let esportes = document.getElementById('esportes');
+    let journal = document.getElementById('journal');
+    let promos = document.getElementById('promos');
+
+    console.log(calcados);
+    console.log(roupas);
+    console.log(acc);
+    console.log(esportes);
+    console.log(journal);
+    console.log(promos);
+
+    calcados.style.display = 'none';
+    roupas.style.display = 'flex';
+    acc.style.display = 'none';
+    esportes.style.display = 'none';
+    journal.style.display = 'none';
+    promos.style.display = 'none';
+}
+
+function acce() {
+    let calcados = document.getElementById('calcados');
+    let roupas = document.getElementById('roupas');
+    let acc = document.getElementById('acessorios');
+    let esportes = document.getElementById('esportes');
+    let journal = document.getElementById('journal');
+    let promos = document.getElementById('promos');
+
+    console.log(calcados);
+    console.log(roupas);
+    console.log(acc);
+    console.log(esportes);
+    console.log(journal);
+    console.log(promos);
+
+    calcados.style.display = 'none';
+    roupas.style.display = 'none';
+    acc.style.display = 'flex';
+    esportes.style.display = 'none';
+    journal.style.display = 'none';
+    promos.style.display = 'none';
+}
+
+function esportes () {
+    let calcados = document.getElementById('calcados');
+    let roupas = document.getElementById('roupas');
+    let acc = document.getElementById('acessorios');
+    let esportes = document.getElementById('esportes');
+    let journal = document.getElementById('journal');
+    let promos = document.getElementById('promos');
+
+    console.log(calcados);
+    console.log(roupas);
+    console.log(acc);
+    console.log(esportes);
+    console.log(journal);
+    console.log(promos);
+
+    calcados.style.display = 'none';
+    roupas.style.display = 'none';
+    acc.style.display = 'none';
+    esportes.style.display = 'flex';
+    journal.style.display = 'none';
+    promos.style.display = 'none';
+}
+
+function journal () {
+    let calcados = document.getElementById('calcados');
+    let roupas = document.getElementById('roupas');
+    let acc = document.getElementById('acessorios');
+    let esportes = document.getElementById('esportes');
+    let journal = document.getElementById('journal');
+    let promos = document.getElementById('promos');
+
+    console.log(calcados);
+    console.log(roupas);
+    console.log(acc);
+    console.log(esportes);
+    console.log(journal);
+    console.log(promos);
+    
+    calcados.style.display = 'none';
+    roupas.style.display = 'none';
+    acc.style.display = 'none';
+    esportes.style.display = 'none';
+    journal.style.display = 'flex';
+    promos.style.display = 'none';
+}
+
+function promos () {
+    let calcados = document.getElementById('calcados');
+    let roupas = document.getElementById('roupas');
+    let acc = document.getElementById('acessorios');
+    let esportes = document.getElementById('esportes');
+    let journal = document.getElementById('journal');
+    let promos = document.getElementById('promos');
+
+    console.log(calcados);
+    console.log(roupas);
+    console.log(acc);
+    console.log(esportes);
+    console.log(journal);
+    console.log(promos);
+
+    calcados.style.display = 'none';
+    roupas.style.display = 'none';
+    acc.style.display = 'none';
+    esportes.style.display = 'none';
+    journal.style.display = 'none';
+    promos.style.display = 'flex';
+}
